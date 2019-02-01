d326cf75f763e5fe4b65b97db8267b6e5f89a706
perhje <38944777+perhje@users.noreply.github.com>
2019-02-01 13:16:18 +0100

    Initial commit

---
+++ app/src/main/java/com/example/phj_1/siemens_kundeoppfoelging/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.phj_1.siemens_kundeoppfoelging;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
