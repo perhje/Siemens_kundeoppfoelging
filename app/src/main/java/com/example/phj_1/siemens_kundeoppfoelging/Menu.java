f1bb949138057a79a60c157ae95becab8118118c
perhje <38944777+perhje@users.noreply.github.com>
2019-02-27 12:41:37 +0100

    rdtu

--- app/src/main/java/com/example/phj_1/siemens_kundeoppfoelging/Menu.java
+++ app/src/main/java/com/example/phj_1/siemens_kundeoppfoelging/Menu.java
@@ -1,12 +1,34 @@
 package com.example.phj_1.siemens_kundeoppfoelging;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Menu extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu);
+    }
+
+    public void serviceRequest(View v){
+        Intent intent=new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void changeUserInformation(View v){
+        Intent intent=new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void youtubeChannel(View v){
+        Intent intent=new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void lifeNet(View v){
+        Intent intent=new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
