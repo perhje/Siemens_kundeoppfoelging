6a90ac1cfe1379489719823681ef93989f1b4cfe
perhje <38944777+perhje@users.noreply.github.com>
2019-04-07 10:26:59 +0200

    QR camera screen

--- app/src/main/java/com/example/phj_1/siemens_kundeoppfoelging/QRScreen.java
+++ app/src/main/java/com/example/phj_1/siemens_kundeoppfoelging/QRScreen.java
@@ -92,13 +92,7 @@ public class QRScreen extends AppCompatActivity {
         imageDimensions = map.getOutputSizes(SurfaceTexture.class)[0];
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
+            ActivityCompat.requestPermissions(QRScreen.this, new String[]{Manifest.permission.CAMERA}, 1);
             return;
         }
         cameraManager.openCamera(cameraId, stateCallback, null);
