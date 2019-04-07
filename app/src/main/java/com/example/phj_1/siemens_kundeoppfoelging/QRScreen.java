0352d15ec0a4664732985432462d6cdd92103023
perhje <38944777+perhje@users.noreply.github.com>
2019-04-07 10:25:47 +0200

    QR camera screen

--- app/src/main/java/com/example/phj_1/siemens_kundeoppfoelging/QRScreen.java
+++ app/src/main/java/com/example/phj_1/siemens_kundeoppfoelging/QRScreen.java
@@ -1,36 +1,213 @@
 package com.example.phj_1.siemens_kundeoppfoelging;
 
 import android.Manifest;
-import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.hardware.Camera;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CameraMetadata;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.StreamConfigurationMap;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Size;
+import android.view.Surface;
+import android.view.TextureView;
 import android.view.View;
-import android.widget.FrameLayout;
 
-public class QRScreen extends AppCompatActivity{
+import java.util.Arrays;
 
-    Camera camera;
-    FrameLayout scanner;
-    ShowCamera showCamera;
+
+
+public class QRScreen extends AppCompatActivity {
+
+    TextureView scanner;
+
+    CameraDevice cameraDevice;
+    String cameraId;
+    Size imageDimensions;
+
+    Handler backgroundHandler;
+    HandlerThread handlerThread;
+
+    CaptureRequest.Builder captureBuilder;
+
+    CameraCaptureSession cameraCaptureSession;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.qrscreen);
-        scanner = (FrameLayout)findViewById(R.id.scanner);
+        scanner = (TextureView) findViewById(R.id.scanner);
         if (ActivityCompat.checkSelfPermission(QRScreen.this,
                 Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(QRScreen.this, new String[]{Manifest.permission.CAMERA},1);
+            ActivityCompat.requestPermissions(QRScreen.this, new String[]{Manifest.permission.CAMERA}, 1);
             return;
         }
-        camera = Camera.open();
+        scanner.setSurfaceTextureListener(surfaceTextureListener);
 
-        showCamera = new ShowCamera(this, camera);
-        scanner.addView(showCamera);
+    }
 
+    TextureView.SurfaceTextureListener surfaceTextureListener = new TextureView.SurfaceTextureListener() {
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+            try {
+                openCamera();
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+            return false;
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+        }
+    };
+
+    private void openCamera() throws CameraAccessException {
+        CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        cameraId = cameraManager.getCameraIdList()[0];
+
+        CameraCharacteristics cameraCharacteristics = cameraManager.getCameraCharacteristics(cameraId);
+        StreamConfigurationMap map = cameraCharacteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+        imageDimensions = map.getOutputSizes(SurfaceTexture.class)[0];
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        cameraManager.openCamera(cameraId, stateCallback, null);
+    }
+
+    CameraDevice.StateCallback stateCallback =new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(@NonNull CameraDevice camera) {
+            cameraDevice = camera;
+            try {
+                startCameraPreview();
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onDisconnected(@NonNull CameraDevice camera) {
+            cameraDevice.close();
+        }
+
+        @Override
+        public void onError(@NonNull CameraDevice camera, int error) {
+            cameraDevice.close();
+            cameraDevice = null;
+        }
+    };
+
+    private void startCameraPreview() throws CameraAccessException{
+        SurfaceTexture texture = scanner.getSurfaceTexture();
+        texture.setDefaultBufferSize(imageDimensions.getWidth(),imageDimensions.getHeight());
+
+        Surface surface = new Surface(texture);
+
+        captureBuilder = cameraDevice.createCaptureRequest(cameraDevice.TEMPLATE_PREVIEW);
+
+        captureBuilder.addTarget(surface);
+
+
+        cameraDevice.createCaptureSession(Arrays.asList(surface), new CameraCaptureSession.StateCallback() {
+            @Override
+            public void onConfigured(@NonNull CameraCaptureSession session) {
+                if(cameraDevice == null){
+                    return;
+                }
+
+                cameraCaptureSession = session;
+                try {
+                    updatePreview();
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+
+            }
+        },null);
+    }
+
+    private void updatePreview() throws CameraAccessException {
+        if(cameraDevice == null){
+            return;
+        }
+
+        captureBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
+        cameraCaptureSession.setRepeatingRequest(captureBuilder.build(),null,backgroundHandler);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startBackgroundThread();
+
+        if(scanner.isAvailable()){
+            try {
+                openCamera();
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }else{
+            scanner.setSurfaceTextureListener(surfaceTextureListener);
+        }
+    }
+
+    private void startBackgroundThread() {
+        handlerThread = new HandlerThread("Camera Background");
+        handlerThread.start();
+
+        backgroundHandler = new Handler(handlerThread.getLooper());
+    }
+
+    @Override
+    protected void onPause() {
+        try {
+            stopBackgroundThread();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        super.onPause();
+    }
+
+    private void stopBackgroundThread() throws InterruptedException {
+        handlerThread.quitSafely();
+        handlerThread.join();
+
+        backgroundHandler = null;
+        handlerThread = null;
     }
 
     public void menu(View v){
